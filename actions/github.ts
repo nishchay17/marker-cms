547778338fc5e4ea979e8d4fab9f1a6941c94f01
Nishchay <nishchay13971@gmail.com>
2023-10-28 03:16:23 +0530

    added add repo form

--- actions/github.ts
+++ actions/github.ts
@@ -1,11 +1,13 @@
 "use server";
 
 import { getServerSession } from "next-auth";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 
 import { authOptions } from "@/lib/auth/auth-options";
 import { db } from "@/lib/db";
 import { accounts, repo } from "@/lib/db/schema";
+import { repoInsertSchema, repoInsertType } from "@/lib/db/schema/repo";
+import { getId } from "@/lib/utils";
 
 async function getCurrentUserId() {
   const userId = (await getServerSession(authOptions))?.user.id;
@@ -26,6 +28,18 @@ async function getCurrentUserAuthToken() {
 
 export async function getRepo() {
   const userId = await getCurrentUserId();
-  const repos = await db.select().from(repo).where(eq(repo.userId, userId));
+  const repos = await db
+    .select()
+    .from(repo)
+    .where(eq(repo.userId, userId))
+    .orderBy(desc(repo.created_at));
   return repos;
+}
+
+export async function createRepo(values: repoInsertType) {
+  const { name } = repoInsertSchema.parse(values);
+  const id = await getId();
+  const userId = await getCurrentUserId();
+  await db.insert(repo).values({ name, id, userId });
+  return { id, name };
 }
--- app/(dashboard)/dashboard/github/page.tsx
+++ app/(dashboard)/dashboard/github/page.tsx
@@ -1,7 +1,74 @@
-type Props = {};
+"use client";
 
-function Github({}: Props) {
-  return <div>Github</div>;
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useQueryClient } from "react-query";
+
+import { createRepo } from "@/actions/github";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import { repoInsertSchema, repoInsertType } from "@/lib/db/schema/repo";
+import { ALL_REPO_KEY } from "@/components/nav/RepoSelector";
+
+function Github() {
+  const queryClient = useQueryClient();
+  const form = useForm<repoInsertType>({
+    resolver: zodResolver(repoInsertSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  async function onSubmit(values: repoInsertType) {
+    try {
+      const newRepo = await createRepo(values);
+      const oldData = queryClient.getQueriesData(ALL_REPO_KEY);
+      const oldRepos = oldData[0][1] as [];
+      queryClient.setQueryData(ALL_REPO_KEY, [newRepo, ...oldRepos]);
+      toast({
+        title: "Success",
+        description: "repo created successfully",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong, please try again later",
+        variant: "destructive",
+      });
+    }
+  }
+
+  return (
+    <div>
+      Github
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Repository's name" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Save</Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
 
 export default Github;
