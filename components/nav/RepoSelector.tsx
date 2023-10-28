dd5db85cf85ab8e43e078b00a2ee31be4440c49e
Nishchay <nishchay13971@gmail.com>
2023-10-28 14:21:55 +0530

    refetch fixes

--- components/nav/RepoSelector.tsx
+++ components/nav/RepoSelector.tsx
@@ -6,11 +6,14 @@ import { getRepo } from "@/actions/github";
 import Select from "../ui/Select";
 
 export const ALL_REPO_KEY = "all-repos";
+const FIVE_MIN = 5 * 60 * 1000;
 
 function RepoSelector() {
   const allRepos = useQuery({
-    queryFn: () => getRepo(),
+    queryFn: getRepo,
     queryKey: ALL_REPO_KEY,
+    staleTime: FIVE_MIN,
+    refetchOnWindowFocus: false,
   });
   return (
     <div className="min-w-[12rem]">
